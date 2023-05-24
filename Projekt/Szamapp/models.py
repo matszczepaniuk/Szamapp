bf3aad29a0f776b2309bf61f211ad66522ac9ae3
Mateusz Szczepaniuk <matszczepaniuk@gmail.com>
2023-05-24 19:13:51 +0200

    Models created

--- Projekt/Szamapp/models.py
+++ Projekt/Szamapp/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=64)
 
 
 class Recipe(models.Model):
@@ -7,19 +12,17 @@ class Recipe(models.Model):
     ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredient")
 
 
-class User(models.Model):
-    username = models.CharField(max_length=32)
-    password = models.CharField(max_length=32)
-    email = models.CharField(max_length=32)
-    favourites = models.ForeignKey(FavouriteRecipes, on_delete=models.CASCADE)
-
-
-class Ingredient(models.Model):
-    name = models.CharField(max_length=64)
+class RecipeIngredient(models.Model):
+    recipe_id = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient_id = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
 
 
 class FavouriteRecipes(models.Model):
     name = models.CharField(max_length=112)
+
+
+class User(User):
+    favourites = models.ForeignKey(FavouriteRecipes, on_delete=models.CASCADE)
 
 
 class Statistics(models.Model):
